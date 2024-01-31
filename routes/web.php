a464fef25277e3dbf35431b57e7b2de3a7a1f521
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-31 23:19:10 +0600

    Routing_01

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//basic route
+Route::get('/test', function(){
+    return 'Hello Sohan';
+});
+
+//route with parameters
+Route::get('/hello/{name}', function($name){
+    return 'Hello, '. $name . '!';
+});
+Route::get('/ami/{nam}', function($nam){
+    return 'Ami, '. $nam . '!';
+});
+
+Route::get('/desh/{zilla}', function($zilla){
+    return 'BD, '. $zilla . '!';
+});
+
+Route::get('/products/{any}', function($any){
+    return 'You are viewing the product:'. $any;
+});
+
+Route::get('/food/{khai}', function($khai){
+    return "I am eating:". $khai;
+});
+//Named Routes
+Route::get('/ami/sohan', function(){
+    return 'ami sohan';
+})->name('bio');
